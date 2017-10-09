fdfc9a87e0f1c6c20053562f8019f6f1127d4bdb
postam95 <postam95@gmail.com>
2017-10-09 20:53:35 +0200

    Navigation between activities, bluetooth implementation

--- app/src/main/java/hu/unideb/inf/obdbypm/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/obdbypm/MainActivity.java
@@ -1,7 +1,22 @@
 package hu.unideb.inf.obdbypm;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import hu.unideb.inf.obdbypm.activities.FaultCodesActivity;
+import hu.unideb.inf.obdbypm.activities.LiveDataActivity;
+import hu.unideb.inf.obdbypm.activities.LongTermDataActivity;
+import hu.unideb.inf.obdbypm.activities.ServiceBookActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +24,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Commit probation
     }
+
+    public void onClickNavigateToLiveDataBtn(View v)
+    {
+        Intent intent = new Intent(this, LiveDataActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickNavigateToFaultCodesBtn(View v)
+    {
+        Intent intent = new Intent(this, FaultCodesActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickNavigateToLongTermDataBtn(View v)
+    {
+        Intent intent = new Intent(this, LongTermDataActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickNavigateToServiceBookBtn(View v)
+    {
+        Intent intent = new Intent(this, ServiceBookActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickNavigateToBluetoothChooseBtn(View v)
+    {
+        ArrayList deviceStrs = new ArrayList();
+        final ArrayList devices = new ArrayList();
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0)
+        {
+            for (BluetoothDevice device : pairedDevices)
+            {
+                deviceStrs.add(device.getName() + "\n" + device.getAddress());
+                devices.add(device.getAddress());
+            }
+        }
+
+        // show list
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.select_dialog_singlechoice,
+                deviceStrs.toArray(new String[deviceStrs.size()]));
+
+        alertDialog.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+                int position = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+                //String deviceAddress = devices.get(position);
+                // TODO save deviceAddress
+            }
+        });
+
+        alertDialog.setTitle("Choose Bluetooth device");
+        alertDialog.show();
+    }
+
+
 }
